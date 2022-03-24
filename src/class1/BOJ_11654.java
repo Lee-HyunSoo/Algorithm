1b6419e863ad0254c6c37f3d99ef61de8e427b95
이현수 <cris_lx@naver.com>
2022-03-24 16:17:32 +0900

    Bronze5

---
+++ src/class1/BOJ_11654.java
@@ -0,0 +1,11 @@
+package class1;
+
+import java.util.Scanner;
+
+public class BOJ_11654 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char input = scan.nextLine().charAt(0);
+        System.out.println((int)input);
+    }
+}
