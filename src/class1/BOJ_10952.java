067e126f46f035220d7c1b0d4fc89f499b828a35
이현수 <cris_lx@naver.com>
2022-03-24 16:09:13 +0900

    Bronze3

---
+++ src/class1/BOJ_10952.java
@@ -0,0 +1,17 @@
+package class1;
+
+import java.util.Scanner;
+
+public class BOJ_10952 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while(true) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            if (a == 0 && b == 0)
+                break;
+            System.out.println(a + b);
+        }
+    }
+}
